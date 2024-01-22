2cefe219ac3b22fbdcf2875144837c5312bebcf9
h-bcode <jon.are.br@gmail.com>
2024-01-22 10:11:21 +0100

    1.0
    File structure

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//import { } from "./pages/..."
+
+// We have to make sure that the right code fires on the right page
+
+//Router - suggested by Oliver:
+
+if (window.location.pathname === "home.html") {
+    homepage();
+}
