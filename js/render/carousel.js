3aa6c2a80347b63edc276c2df77d751b8bc29164
jonhavbra87 <jon.are.br@gmail.com>
2024-02-02 09:54:38 +0100

    v.179

--- js/render/carousel.js
+++ js/render/carousel.js
@@ -1,13 +1,17 @@
 export function renderCarousel(postData) {
     const blogContainer = document.querySelector(".carousel");
-    if (!blogContainer) {
-        console.error("Blog container not found for the post: ", postData);
-        return;
-    }
+    // if (!blogContainer) {
+    //     console.error("Blog container not found for the post: ", postData);
+    //     return;
+    // }
 
-    // Creating the container for each blog post
-    const postContainer = document.createElement("div");
-    postContainer.classList.add("carousel-container");
+    // Creating the container for each blog post (ul element)
+    const postContainer = document.createElement("ul");
+    postContainer.classList.add("carousel-track-container");
+
+    // Creating the list item (li element)
+    const listItem = document.createElement("li");
+    listItem.classList.add("carousel-slide");
 
     // Adding the featured image if available
     const featuredMedia = postData._embedded?.['wp:featuredmedia']?.[0]?.source_url;
@@ -16,23 +20,25 @@ export function renderCarousel(postData) {
         img.src = featuredMedia;
         img.alt = postData.title.rendered;
         img.classList.add("blog-post-image");
-        postContainer.append(img);
+        listItem.append(img); // Appending to the list item
     }
 
     // Adding the title
     const title = document.createElement("h3");
     title.textContent = postData.title.rendered;
     title.classList.add("blog-post-title");
-    postContainer.append(title);
+    listItem.append(title); // Appending to the list item
 
     // Adding a read more button (anchortag)
     const readMoreButton = document.createElement("a");
     readMoreButton.href = "/blogdetails/?id=" + postData.id;
     readMoreButton.textContent = "Read More";
     readMoreButton.classList.add("blog-post-button");
-    postContainer.append(readMoreButton);
+    listItem.append(readMoreButton); // Appending to the list item
 
+    // Appending the list item to the post container (ul element)
+    postContainer.append(listItem);
 
-    // Appending the blog post container to the main blog container
+    // Appending the blog post container (ul element) to the main blog container
     blogContainer.append(postContainer);
 }
