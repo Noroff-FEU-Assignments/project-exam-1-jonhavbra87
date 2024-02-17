776c81e5d0b4873d9bac60997ee3f607d72896c2
h-bcode <jon.are.br@gmail.com>
2024-02-17 13:33:48 +0100

    updated accessibility
    added <title> to iframe, hamburgermenu and dots in carousel

--- js/ui/carousel/carousel.js
+++ js/ui/carousel/carousel.js
@@ -17,7 +17,7 @@ const slider = function () {
       slides.forEach(function (_, i) {
         dotContainer.insertAdjacentHTML(
           'beforeend',
-          `<button class="dots__dot" data-slide="${i}"></button>`
+          `<button class="dots__dot" data-slide="${i}" title="Gå til lysbilde ${i + 1}"></button>`
         );
       });
     };
